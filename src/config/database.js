b715644819390511eefbdead5f1d96f2dd8433f7
Rogério Bonfim <rogeriomateus@icloud.com>
2020-08-05 16:53:14 -0300

    url to connect

--- src/config/database.js
+++ src/config/database.js
@@ -7,10 +7,8 @@ let database
 if (process.env.DATABASE_URL) {
     // the application is executed on Heroku ... use the postgres database
     database = {
-        use_env_variable:
-            'postgres://xsmwpiwdiswucr:5f11c61ab31ba705b3bfe5e84a7455344f2b5eb8b16ea2181643df88d706eddd@ec2-52-72-221-20.compute-1.amazonaws.com:5432/d3gp5kd23selll',
-        url:
-            'postgres://xsmwpiwdiswucr:5f11c61ab31ba705b3bfe5e84a7455344f2b5eb8b16ea2181643df88d706eddd@ec2-52-72-221-20.compute-1.amazonaws.com:5432/d3gp5kd23selll',
+        use_env_variable: process.env.DATABASE_UR,
+        url: process.env.DATABASE_UR,
         dialect: process.env.DB_DIALECT || 'postgres',
         dialectOptions: {
             ssl: {
@@ -18,10 +16,10 @@ if (process.env.DATABASE_URL) {
                 rejectUnauthorized: false,
             },
         },
-        host: 'ec2-52-72-221-20.compute-1.amazonaws.com',
-        username: 'xsmwpiwdiswucr',
-        password: '5f11c61ab31ba705b3bfe5e84a7455344f2b5eb8b16ea2181643df88d706eddd',
-        database: 'd3gp5kd23selll',
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
         storage: './__tests__/database.sqlite',
         ssl: true,
         options: {
