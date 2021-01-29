8156ff735c07c5e65ff69f0dc90cc7a5a7dd6846
Rogerio Bonfim <rogeriobonfim@iMac-de-Rogerio.local>
2021-01-29 17:04:54 -0300

    up

--- src/models/seeders/20210127201728-user.js
+++ src/models/seeders/20210127201728-user.js
@@ -15,6 +15,6 @@ module.exports = {
         ])
     },
     down: (queryInterface, Sequelize) => {
-        return queryInterface.bulkDelete('Users', null, {})
+        return queryInterface.bulkDelete('users', null, {})
     },
 }
