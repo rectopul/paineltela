9436585cabc34eed68c9a02b2eb977f2fc589cd9
Neide Dev <neidepereiraantunes@outlook.com>
2022-01-17 11:44:43 -0300

    confirm configs

--- src/config/database.js
+++ src/config/database.js
@@ -4,52 +4,22 @@ require('dotenv').config({
 
 let database
 
-if (process.env.DATABASE_URL) {
-    // the application is executed on Heroku ... use the postgres database
-    database = {
-        use_env_variable: process.env.DATABASE_UR,
-        url: process.env.DATABASE_UR,
-        dialect: process.env.DB_DIALECT || 'postgres',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
-        host: process.env.DB_HOST,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        storage: './__tests__/database.sqlite',
-        ssl: true,
-        options: {
-            port: 5432,
-        },
-        define: {
-            timestamps: true,
-            underscored: true,
-            underscoredAll: true,
-        },
-    }
-} else {
-    // the application is executed on the local machine ... use mysql
-    database = {
-        dialect: process.env.DB_DIALECT || 'postgres',
-        host: process.env.DB_HOST,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        storage: './__tests__/database.sqlite',
-        logging: false,
-        options: {
-            port: 5432,
-        },
-        define: {
-            timestamps: true,
-            underscored: true,
-            underscoredAll: true,
-        },
-    }
+database = {
+    dialect: process.env.DB_DIALECT || 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    storage: './__tests__/database.sqlite',
+    logging: false,
+    options: {
+        port: 5432,
+    },
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
 }
 
 module.exports = database
