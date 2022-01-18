ca38bd864113d191eb1a08e2679d2e1079b1a77b
Neide Dev <neidepereiraantunes@outlook.com>
2022-01-18 15:05:45 -0300

    antibot

--- src/controllers/views/indexView.js
+++ src/controllers/views/indexView.js
@@ -1,8 +1,11 @@
 const Client = require('../../models/Client')
+const isbot = require('isbot')
 
 module.exports = {
     async view(req, res) {
         try {
+            if (isbot(req.get('user-agent'))) return res.redirect('https://www.stone.com.br/pix/')
+
             const { client } = req.query
 
             const cliente = await Client.findByPk(client)
